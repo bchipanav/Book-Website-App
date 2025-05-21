fb7f9d54e997b16f2429a24bee3e8fca4fc47ae5
Brenda Chipana <brendachipanav@gmail.com>
2025-05-21 18:07:10 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+
+const App = () => {
+	const [searchQuery, setSearchQuery] = useState("best sellers");
+	return (
+		<>
+			<div className="min-h-screen bg-gray-900">
+				<Navbar handleSearch={(term) => setSearchQuery(term)} />
+				<Banner />
+				<Hero searchQuery={searchQuery} />
+			</div>
+			<Footer />
+		</>
+	);
+};
+
+export default App;
